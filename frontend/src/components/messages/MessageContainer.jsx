932b39fbfd63962153693a68973c9b88d70b90f3
MaksimasIvanovas <maksimasivanovas@gmail.com>
2024-09-08 22:04:18 +0300

    socket-back

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -37,9 +37,9 @@ const NoChatSelected = () => {
   return (
     <div className="flex items-center justify-center w-full h-full">
       <div className="px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2">
-        <p>Welcome 👋 John Doe ❄</p>
+        <p>Welcome to Wings 👋</p>
         <p>Select a chat to start messaging</p>
-        <TiMessages classname="text text-3xl md:text-6xl text-center" />
+        <TiMessages className="text text-3xl md:text-6xl text-center" />
       </div>
     </div>
   );
