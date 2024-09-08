c6e9bdecb9ac5a6fbea1fdc81e26274e00cc4b1a
MaksimasIvanovas <maksimasivanovas@gmail.com>
2024-09-08 23:37:44 +0300

    final-touch

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,15 +1,17 @@
-import Message from "./Message";
+import { useEffect, useRef } from "react";
 import useGetMessages from "../../hooks/useGetMessages";
 import MessageSkeleton from "../skeletons/MessageSkeleton";
-import { useEffect, useRef } from "react";
+import Message from "./Message";
+import useListenMessages from "../../hooks/useListenMessages";
 
 const Messages = () => {
   const { messages, loading } = useGetMessages();
+  useListenMessages();
   const lastMessageRef = useRef();
 
   useEffect(() => {
     setTimeout(() => {
-      lastMessageRef.current?.scrollIntoView({ behaviour: "smooth" });
+      lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
     }, 100);
   }, [messages]);
 
@@ -19,13 +21,13 @@ const Messages = () => {
         messages.length > 0 &&
         messages.map((message) => (
           <div key={message._id} ref={lastMessageRef}>
-            <Message key={message._id} message={message} />
+            <Message message={message} />
           </div>
         ))}
 
       {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
       {!loading && messages.length === 0 && (
-        <p className="text-center">Say Something!</p>
+        <p className="text-center">Send a message to start the conversation</p>
       )}
     </div>
   );
